54942158f344363deebab9e4eb038d48297199dd
goroyabu <goro.yabu@ipmu.jp>
2019-06-25 16:26:10 +0900

    update

--- com_cli.rb
+++ com_cli.rb
@@ -3,7 +3,6 @@ class ComCli < Formula
   homepage "https://github.com/goroyabu/com_cli"
   url "https://github.com/goroyabu/com_cli/archive/1.1.2.zip"
   version "1.1.2"
-  sha256 "c03b008a4a993798056ca1e92d4108af3645d6eb0b6641ccc7fc7c84bf886d9e"
   depends_on "make" => :build
 
   def install

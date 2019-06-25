865bd9a518921fa8553ac5b8bd6fe8af88f2c0a1
goroyabu <goro.yabu@ipmu.jp>
2019-06-25 16:20:39 +0900

    update

--- com_cli.rb
+++ com_cli.rb
@@ -1,9 +1,9 @@
 class ComCli < Formula
   desc "C++Library of CLI"
   homepage "https://github.com/goroyabu/com_cli"
-  url "https://github.com/goroyabu/com_cli"
+  url "https://github.com/goroyabu/com_cli/archive/1.1.2.zip"
   version "1.1.2"
-  sha256 "ff2e4a566744c16d51616d94ac6546a54e4d5a4430516197de468bb3652748da"
+  sha256 "c03b008a4a993798056ca1e92d4108af3645d6eb0b6641ccc7fc7c84bf886d9e"
   depends_on "make" => :build
 
   def install

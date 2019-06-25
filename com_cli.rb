ebea58c09d06aa3ee1dd7d2ca61d7ff31fc1781c
goroyabu <goro.yabu@ipmu.jp>
2019-06-25 14:40:49 +0900

    update

--- com_cli.rb
+++ com_cli.rb
@@ -16,8 +16,9 @@ class ComCli < Formula
     #                      "--disable-silent-rules",
     #                      "--prefix=#{prefix}"
     # system "cmake", ".", *std_cmake_args
-    system "make install"
-    bin.install "lib/libCOMCLI.so" # if this fails, try separate make/make install steps
+    system "echo test"
+    #system "make", "install"
+    #bin.install "lib/libCOMCLI.so" # if this fails, try separate make/make install steps
   end
 
   test do

f69a06a0934b6a2bec12e605097fd811fa1d8df9
goroyabu <goro.yabu@ipmu.jp>
2019-06-25 15:02:20 +0900

    update

--- com_cli.rb
+++ com_cli.rb
@@ -1,36 +1,17 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class ComCli < Formula
-  desc ""
-  homepage ""
+  desc "C++Library of CLI"
+  homepage "https://github.com/goroyabu/com_cli"
   url "https://github.com/goroyabu/com_cli"
+  version "1.1.2"
   sha256 "ff2e4a566744c16d51616d94ac6546a54e4d5a4430516197de468bb3652748da"
   depends_on "make" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    #system "./configure", "--disable-debug",
-    #                      "--disable-dependency-tracking",
-    #                      "--disable-silent-rules",
-    #                      "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "echo test"
-    #system "make", "install"
-    #bin.install "lib/libCOMCLI.so" # if this fails, try separate make/make install steps
+    system "echo", "install..."
+    system "make", "install"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test com`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end

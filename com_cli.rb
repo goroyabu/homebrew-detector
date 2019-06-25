ebbfe9950d0cd5cf5e508e005c078ca6952dea8a
goroyabu <goro.yabu@ipmu.jp>
2019-06-25 18:19:22 +0900

    update

--- com_cli.rb
+++ com_cli.rb
@@ -1,8 +1,8 @@
 class ComCli < Formula
   desc "C++ Functions for Command Line Interface"
   homepage "https://github.com/goroyabu/com_cli"
-  url "https://github.com/goroyabu/com_cli/archive/1.1.2.zip"
-  version "1.1.2"
+  url "https://github.com/goroyabu/com_cli/archive/1.1.3.zip"
+  version "1.1.3"
   depends_on "make" => :build
   depends_on "gcc" => :build
 
